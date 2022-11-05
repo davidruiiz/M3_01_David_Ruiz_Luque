a8b1f015ef3c5aaf70317575e4742c553d49cb66
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-11-05 19:42:02 +0100

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,22 +8,6 @@ Experimentación
 Crea algunos productos
 Prueba a mostrar los datos de algun producto y a modificar algun valor, por ejemplo, prueba a modificar el precio de un producto
 '''
-#Creación
-class Alumno():
-
-    def __init__(self, nombre, nota):
-        self.nombre = nombre
-        self.nota = nota
-        print('\nEl alumno se ha creado con éxito.')
-
-    def __str__(self):
-        return '\nEl alumno {} ha sacado un {}.'.format(self.nombre, self.nota)
-
-    def calificacion(self):
-        if self.nota >= 5:
-            return print('\n{} ha aprobado con un {}.'.format(self.nombre, self.nota))
-        else:
-            return print('\n{} ha suspendido con un {}.'.format(self.nombre, self.nota))
 
 class Producto():
    
@@ -33,11 +17,8 @@ class Producto():
         self.precio = precio
         self.tipo = tipo
         print('El constructor se ha creado con éxito.')
+
+    def __str__(self):
+
 #Experimentación
 
-alumno1=Alumno('David', 8)
-alumno2=Alumno('Victor', 4)
-print(alumno1)
-print(alumno2)
-Alumno.calificacion(alumno1)
-Alumno.calificacion(alumno2)
